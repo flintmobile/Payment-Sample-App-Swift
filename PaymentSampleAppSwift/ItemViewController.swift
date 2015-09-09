3ef40f75bcce868d4eec31e25db1d32863888054
PC Nguyen <pc@flint.com>
2015-09-09 12:45:51 -0700

    initial project setup

---
+++ PaymentSampleAppSwift/ItemViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ItemViewController.swift
+//  PaymentSampleAppSwift
+//
+//  Created by PC on 9/9/15.
+//  Copyright © 2015 PC. All rights reserved.
+//
+
+import UIKit
+
+class ItemViewController : UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+}
